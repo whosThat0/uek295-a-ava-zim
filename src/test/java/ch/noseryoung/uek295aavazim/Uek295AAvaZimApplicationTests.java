f201b57d0b92c4ae0ea30f37b6b22ad6f02c6691
whosThat0 <ava1zimmermann@gmail.com>
2025-03-28 11:03:11 +0100

    initial commit

---
+++ src/test/java/ch/noseryoung/uek295aavazim/Uek295AAvaZimApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.noseryoung.uek295aavazim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uek295AAvaZimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
